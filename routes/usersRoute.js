a3092cd3e07155c5c2701a438faadf947b2f8759
Oscar vallin <arnoldvallin@gmail.com>
2020-12-29 18:56:15 -0600

    fix production

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -5,13 +5,15 @@ const {check} = require('express-validator');
 const {createUser} = require('../controller/userController');
 
 
-route.post('/',
-    [
-        check('name', 'the name is required').not().isEmpty(),
-        check('email', 'must add a valid email').isEmail(),
-        check('password', 'your password must be at leats 6 characters').isLength({min: 6})
-    ],
-    createUser
+route.post('/',(req,res ) => {
+    res.json(req.body)
+}
+    // [
+    //     check('name', 'the name is required').not().isEmpty(),
+    //     check('email', 'must add a valid email').isEmail(),
+    //     check('password', 'your password must be at leats 6 characters').isLength({min: 6})
+    // ],
+    // createUser
 );
 
 module.exports = route;
