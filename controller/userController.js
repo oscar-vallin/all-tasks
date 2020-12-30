1734ae4f5bd5d8a4ea43a19bb1bd0ef8b886d703
Oscar vallin <arnoldvallin@gmail.com>
2020-12-29 19:01:45 -0600

    fix production

--- controller/userController.js
+++ controller/userController.js
@@ -6,47 +6,47 @@ const key = require('../config/keys');
 const {validationResult} = require('express-validator');
 
 exports.createUser = async (req,res) => {
-    res.json(req.body)
+    
     const {password, email} = req.body;
     //check user validate
-  
+    
     const err = validationResult(req);
     
     if(!err.isEmpty()) return res.status(400).json({err: err.array()});
-
-    try {
+    res.json(req.body)
+    // try {
      
-        let user;
+    //     let user;
 
-        user = await User.findOne({email});
+    //     user = await User.findOne({email});
         
-        if(user) return res.status(400).json({msg: "This user is already exist"});
+    //     if(user) return res.status(400).json({msg: "This user is already exist"});
         
-        user = await User(req.body);
+    //     user = await User(req.body);
         
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
+    //     const salt = await bcrypt.genSalt(10);
+    //     user.password = await bcrypt.hash(password, salt);
     
-        await user.save();
+    //     await user.save();
 
-        const payload = {
-            user : {
-                id: user.id
-            }
-        };
-        jwt.sign(payload, key.secretWord, {
-            expiresIn: 3600
-        }, (error, token) => {
-            if(error) throw error;
+    //     const payload = {
+    //         user : {
+    //             id: user.id
+    //         }
+    //     };
+    //     jwt.sign(payload, key.secretWord, {
+    //         expiresIn: 3600
+    //     }, (error, token) => {
+    //         if(error) throw error;
 
-            //confirm
-            res.json({token})
-            console.log(req.body)
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({msg: `there was an error ${req.body}`});
-    }
+    //         //confirm
+    //         res.json({token})
+    //         console.log(req.body)
+    //     });
+    // } catch (error) {
+    //     console.log(error);
+    //     return res.status(400).json({msg: `there was an error ${req.body}`});
+    // }
     
    
 }
